158bf772bd4c855671013833a5f623b52acc3011
justyna sojka <s26061@pjwstk@edu.pl>
2021-11-16 23:33:44 +0100

    zaktualizowała zadania

--- src/s01-hello-name.cpp
+++ src/s01-hello-name.cpp
@@ -3,9 +3,9 @@
 
 auto main() -> int
 {
-auto name = std::string{};
-std::getline(std::cin, name);
-
-std::cout << "Hello, " << name << "! \n";
-return 0;
+     auto name = std::string{};
+     std::cout << "What's your name? \n";
+     std::getline(std::cin, name);
+     std::cout << "Hello, " << name << "! \n";
+     return 0;
 }
--- src/s01-mul.cpp
+++ src/s01-mul.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <string>
 
-auto main(int, char* argv[]) -> int
+auto main(int argc, char* argv[]) -> int
 {
-auto const a=std::stoi(argv[1]);
-auto const b=std::stoi(argv[2]);
-std::cout << (a * b) << "\n";
-return 0;
+   if (argc < 3)
+    { std::cerr << "Błąd, podaj dwie liczby! \n";
+      return 1;
+    }
+       
+   auto const a=std::stoi(argv[1]);
+   auto const b=std::stoi(argv[2]); 
+   std::cout << (a * b) << "\n";
+   return 0;
 }
