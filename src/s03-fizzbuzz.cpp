bf6949a8c3bcc363ea3507550abfa7e79af38378
justyna sojka <s26061@pjwstk@edu.pl>
2021-11-18 00:43:55 +0100

    poprawiłam formatowanie

--- src/s03-fizzbuzz.cpp
+++ src/s03-fizzbuzz.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
 #include <string>
 
-auto get_number_from_user(std::string prompt) -> int
-{ 
-    auto n = std::string{};
-    std::cout << prompt;
-    std::getline(std::cin, n);
-   return std::stoi(n);
+auto get_number_from_user(std::string prompt) -> int {
+  auto n = std::string{};
+  std::cout << prompt;
+  std::getline(std::cin, n);
+  return std::stoi(n);
 }
 
-
-    auto main() -> int
-{
-   auto n = get_number_from_user("Podaj liczbę: ");
-   if (n % 3 == 0 && n % 5 != 0) {
-   std::cout << "Fizz\n"; 
+auto main() -> int {
+  auto n = get_number_from_user("Podaj liczbę: ");
+  if (n % 3 == 0 && n % 5 != 0) {
+    std::cout << "Fizz\n";
   }
-   if (n % 5 == 0 && n % 3 != 0) {
-   std::cout << "Buzz\n";
+  if (n % 5 == 0 && n % 3 != 0) {
+    std::cout << "Buzz\n";
   }
-   if (n % 3 == 0 && n % 5 == 0) {
-  std::cout << "FizzBuzz\n";
- }
- 
-return 0;
+  if (n % 3 == 0 && n % 5 == 0) {
+    std::cout << "FizzBuzz\n";
+  }
 
+  return 0;
 }
-
-
-    
