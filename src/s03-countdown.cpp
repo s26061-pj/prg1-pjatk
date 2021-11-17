77515e0b0ef50d986824347ed358c611cff0f045
justyna sojka <s26061@pjwstk@edu.pl>
2021-11-18 00:33:29 +0100

    poprawiłam formatowanie

--- src/s03-countdown.cpp
+++ src/s03-countdown.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 #include <string>
 
-auto get_number_from_user(std::string prompt) -> int
-{
-    auto n = std::string{};
-    std::cout << prompt;
-    std::getline(std::cin, n);
-    return std::stoi(n);
+auto get_number_from_user(std::string prompt) -> int {
+  auto n = std::string{};
+  std::cout << prompt;
+  std::getline(std::cin, n);
+  return std::stoi(n);
 }
 
-auto main() -> int
-{ 
-   auto n = get_number_from_user("Podaj liczbę: ");
-   
+auto main() -> int {
+  auto n = get_number_from_user("Podaj liczbę: ");
+
   while (n > 0) {
-  std::cout << n-- << "...\n";
- }
-  
-  while (n < 0 ) {
-  std::cout << n++ << "...\n";
- } 
-  
- std::cout << n << "...\n";
+    std::cout << n-- << "...\n";
+  }
+
+  while (n < 0) {
+    std::cout << n++ << "...\n";
+  }
+
+  std::cout << n << "...\n";
 
   return 0;
 }
