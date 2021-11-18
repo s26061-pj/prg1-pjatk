0017f4ce644030b9bb4bfd86360116ff737afb07
justyna sojka <s26061@pjwstk@edu.pl>
2021-11-18 17:56:44 +0100

    poprawiłam program

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 #include <string>
 
-auto number_of_bottles(std::string prompt) -> int {
-  auto bottle = std::string{};
-  std::cout << prompt;
-  std::getline(std::cin, bottle);
-  return std::stoi(bottle);
-}
-
-auto main() -> int {
-  auto n = number_of_bottles("Ile masz flaszek:");
-  while (n > 0) {
-    std::cout << n << " bottles of beer on the wall, ";
-    std::cout << n << " bottles of beer.\n";
+auto main(int argc, char *argv[]) -> int {
+  auto bottles = 99;
+  if (argc > 1) {
+    bottles = std::stoi(argv[1]);
+  }
+  for (auto n = bottles; n > 0; --n) {
+    std::cout << bottles << " bottles of beer on the wall, ";
+    std::cout << bottles << " bottles of beer.\n";
     std::cout << "Take one down, pass it around, ";
-    std::cout << --n << " bottles of beer on the wall...\n";
+    std::cout << --bottles << " bottles of beer on the wall...\n";
   }
   std::cout << "No more bottles of beer on the wall,";
   std::cout << "no more bottles of beer.";
   std::cout << "Go to the store and buy some more, ";
-  std::cout << n << " bottles of beer on the wall...\n";
+  std::cout << bottles << " bottles of beer on the wall...\n";
 
   return 0;
 }
