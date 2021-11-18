79047627fcbd0e2f7a71cd6d439176123cbbfdb6
justyna sojka <s26061@pjwstk@edu.pl>
2021-11-18 13:19:23 +0100

    poprawialam formatowanie

--- src/s01-sub.cpp
+++ src/s01-sub.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <string>
-auto main(int argc, char* argv[]) -> int
-{
-    
-   if (argc < 3)
-    { std::cerr << "Błąd, podaj dwie liczby! \n";
-      return 1;
-    }
-    auto const a=std::stoi(argv[1]);
-    auto const b=std::stoi(argv[2]);
-    std::cout << (a - b) << "\n";
-    return 0;
+auto main(int argc, char *argv[]) -> int {
+
+  if (argc < 3) {
+    std::cerr << "Błąd, podaj dwie liczby! \n";
+    return 1;
+  }
+  auto const a = std::stoi(argv[1]);
+  auto const b = std::stoi(argv[2]);
+  std::cout << (a - b) << "\n";
+  return 0;
 }
